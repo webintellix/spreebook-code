5eef2819327ed9672859294e2f2899d9275a178c
Rupak Ganguly <rupakg@gmail.com>
2013-09-19 22:38:22 -0400

    Initial code after generating the sportsmall Spree app.

---
+++ apps/sportsmall/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportsmall::Application.config.secret_token = '9fe1aeb8626153efae33560db7cf769d9608d5a599e329a406845b6f23eb63c4b22afd05ed52c1bb5b3398d9f3c01b1351279f124fa36ffd5ee79c061ed8c84f'
