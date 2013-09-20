52b04412a107630127252149ebb455c4105b171f
Rupak Ganguly <rupakg@gmail.com>
2013-09-19 22:32:52 -0400

    Add Spree demo store app.

---
+++ apps/demostore/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demostore::Application.config.secret_token = '831e522674c710c6a066af3350d243bc96bdd4e4b3d279b4504c112ed24bd86f1a3d7a3ee52c1594faf7b0fd2101655d6dd64de9485d6c5bbb5dbe41e66a5355'
